7359db8ef108f47c443c942f3cdb2c6e6e14880d
DurgeshKumar143 <dk0078774@gmail.com>
2024-09-23 08:53:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const http = require("http");
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const router = require('./routes');
+const bodyParser = require('body-parser');
+const {Server} = require("socket.io");
+
+
+ 
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials : true,
+}));
+
+
+app.get("",(req,res)=>{
+    res.send("Ecommerce API is running.....");
+});
+
+
+
+
+app.use(express.json());
+app.use(express.static("./invoices"));
+app.use(cookieParser());
+app.use(bodyParser.json({limit:'20mb'}))
+app.use(bodyParser.urlencoded({limit:'20mb',extended:true}))
+
+app.use("/api",router);
+
+
+
+
+
+const PORT =  process.env.PORT || 8200
+
+
+connectDB().then(()=>{
+    server.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+        console.log("Server is Running No problems in then running server");
+    })
+}).catch((err)=>{
+    
+    console.log("Error connecting",err)
+
+});
+
+
+
+
